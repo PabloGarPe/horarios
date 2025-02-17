00e29bcd71c51881f31587b440ff6f3e3292314f
Pablo <pablogarciapernas@gmail.com>
2025-02-17 19:39:18 +0100

    El fetch listo, tengo q subirlo a render y probar estilos

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return ( 
+        <header>
+            <div className="bg-divs-200 w-full min-h-[10vh] fixed z-0 dark:bg-divs-600">
+                <div className="flex flex-col justify-center items-center h-full py-3">
+                    <h1 className="text-3xl font-bold italic">HORARIO PCEO MATEMÁTICAS E INFORMÁTICA 2º</h1>
+                </div>
+            </div>
+        </header>
+    )
+}
