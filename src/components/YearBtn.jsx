4ed9b0eebcd289e5e56a3383c24f9162fd145bc7
mendana <diegodiazmenda@gmail.com>
2025-02-28 17:57:57 +0100

    The btn styles are finished

--- src/components/YearBtn.jsx
+++ src/components/YearBtn.jsx
@@ -13,7 +13,7 @@ export const YearBtn = ({ year }) => {
     return (
         <button className="bg-black rounded-none mx-2" onClick={() => yearClicked(clicked)}>
             <span className={` block px-4 py-2  truncate
-   border-black border-2 rounded-none text-xl hover:translate-x-0 hover:translate-y-0 transition-all font-mono font-bold ${clicked ? "bg-red-500 text-black" : "bg-white -translate-x-2 -translate-y-2"}`}>
+   border-black border-2 rounded-none text-xl hover:translate-x-0 hover:translate-y-0 transition-all font-mono font-bold -translate-x-2 -translate-y-2 ${clicked ? "bg-red-500 text-black" : "bg-white"}`}>
                 Year {year}
             </span>
         </button>
