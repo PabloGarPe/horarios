a134b579a4df86c5942fc75414a0dd048801b484
Pablo <pablogarciapernas@gmail.com>
2025-02-28 17:43:05 +0100

    a

--- src/components/YearBtn.jsx
+++ src/components/YearBtn.jsx
@@ -12,8 +12,8 @@ export const YearBtn = ({ year }) => {
 
     return (
         <button className="bg-black rounded-none mx-2">
-            <span className={`bg-white block px-4 py-2 -translate-x-2 truncate
-  -translate-y-2 border-black border-2 rounded-none text-xl hover:translate-x-0 hover:translate-y-0 transition-all font-mono font-bold ${clicked ? "active:bg-red-600 active:transition-colors active:duration-200" : ""}`} onClick={() => yearClicked(clicked)}>
+            <span className={` block px-4 py-2  truncate
+   border-black border-2 rounded-none text-xl hover:translate-x-0 hover:translate-y-0 transition-all font-mono font-bold ${clicked ? "bg-red-600 transition-colors duration-200" : "bg-white -translate-x-2 -translate-y-2"}`} onClick={() => yearClicked(clicked)}>
                 Year {year}
             </span>
         </button>
