c91cc3bf7cc851e24f0f0e778bb2c4a238620706
mendana <diegodiazmenda@gmail.com>
2025-03-06 16:19:58 +0100

    Trying the dark theme config

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 import './styles/headerNav.css'
 import { WeekSelector } from './WeekSelector'
 import { MainBtn } from './MainBtn'
+import { ThemeBtn } from './ThemeBtn'
 
 export const HeaderNav = ({ }) => {
     return (
-        <section className="main-wrapper bg-white border-b-8 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] p-4 sm:px-6 sticky top-0 z-10" >
+        <section className="main-wrapper bg-white border-b-8 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] p-4 sm:px-6 sticky top-0 z-10 dark:bg-red" >
             <div className="title-div">
                 <h1 className="text-3xl sm:text-4xl font-extrabold font-mono">PCEO Informática + Matemáticas</h1>
-                <MainBtn>
+                <ThemeBtn>
                     Theme
-                </MainBtn>
+                </ThemeBtn>
             </div>
             <section className='inf-wrapper'>
                 <div className="btns-div">
---
+++ src/components/ThemeBtn.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import './styles/mainBtn.css'
+import { useTheme } from "./ThemeContext"
+
+
+export const ThemeBtn = ({children}) => {
+    const [clicked, setClicked] = useState(false)
+
+    const btnClicked = (newClicked) => {
+        newClicked = !newClicked
+        setClicked(newClicked)
+    }
+
+    const { theme, toggleTheme } = useTheme()
+
+    const handleThemeChange = () => {
+        toggleTheme()
+        document.documentElement.classList.toggle(theme)
+    }
+
+    const triggerFunctions = (clicked) => {
+        handleThemeChange();
+        btnClicked(clicked);
+    }
+
+    return (
+        <button className="bg-black rounded-none mx-2" onClick={() => triggerFunctions(clicked)}>
+            <span className={` block px-4 py-2  truncate
+   border-black border-2 rounded-none text-xl hover:translate-x-0 hover:translate-y-0 transition-all font-mono font-bold -translate-x-2 -translate-y-2 ${clicked ? "btn-clicked" : "bg-white"}`}>
+                {children}
+            </span>
+        </button>
+    )
+}
