cf9f79ba660c01c287fafe8fde9aaac827b5d928
mendana <diegodiazmenda@gmail.com>
2025-03-07 17:12:48 +0100

    Nothing important

--- src/utils/subjectColors.js
+++ src/utils/subjectColors.js
@@ -1,4 +1,10 @@
 export const subjectColors = {
+    //1 PCEO
+    'Ondas': 'bg-blue-500 border-blue-800',
+    'Empresa': 'bg-green-500 border-green-800',
+
+
+    //2 PCEO
     'FCR': 'bg-red-500 border-red-800',
     'Análisis I': 'bg-blue-500 border-blue-800',
 }
