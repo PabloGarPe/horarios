496254dad59bbef7be74b80b776fb53d434bc129
mendana <diegodiazmenda@gmail.com>
2025-03-13 16:41:04 +0100

    Need to fix the scroll behaviour

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -14,26 +14,32 @@ export const HeaderNav = ({ }) => {
     return (
         <section className="flex flex-col bg-white border-b-8 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] p-4 sm:px-6 sticky top-0 z-10 dark:bg-gray-800" >
             <div className="flex flex-col md:flex-row justify-between items-center md:pb-[20px]">
-                <h1 className="pb-5 md:pb-0 lg:text-3xl text-2xl font-extrabold font-mono dark:text-white">PCEO Informática + Matemáticas</h1>
+                <h1 className="text-center md:text-start pb-5 md:pb-0 lg:text-3xl text-2xl font-extrabold font-mono dark:text-white">PCEO Informática + Matemáticas</h1>
                 <ThemeBtn>
                     Theme
                 </ThemeBtn>
             </div>
-            <section className='pt-5 md:pt-0 flex md:flex-row md:justify-between items-center md:gap-[1em] gap-[1.5em] flex-col '>
-                <div className="flex flex-row justify-between lg:gap-[1em] gap-[0.5em] items-center">
-                    {years.map((year, index) => (
-                        <MainBtn 
-                            key={index}
-                            label={year}
-                            isActive={activeYear === index}
-                            onClick={() => setActiveYear(index)}
-                        ></MainBtn>
-                    ))}
+            <section className="pt-5 md:pt-0 flex md:flex-row md:justify-between items-center md:gap-[1em] gap-[1.5em] flex-col">
+                {/* Contenedor con scroll horizontal solo para los botones */}
+                <div className="w-full overflow-x-auto whitespace-nowrap flex justify-center md:justify-start">
+                    <div className="flex lg:gap-[1em] gap-[0.5em] items-center pt-[10px] pl-4 md:pl-0 pr-4">
+                        {years.map((year, index) => (
+                            <MainBtn
+                                key={index}
+                                label={year}
+                                isActive={activeYear === index}
+                                onClick={() => setActiveYear(index)}
+                            />
+                        ))}
+                    </div>
                 </div>
-                <div>
-                    <WeekSelector/>
+
+                {/* Contenedor del selector de semana, centrado en vista de columna */}
+                <div className="flex justify-center w-full md:w-auto md:justify-end">
+                    <WeekSelector />
                 </div>
             </section>
+
         </section>
     )
 }
