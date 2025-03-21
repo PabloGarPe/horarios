59387d1e90a3b584e929953b92fada433480c59b
mendana <diegodiazmenda@gmail.com>
2025-03-21 16:15:42 +0100

    More modeling

--- src/utils/dataParser.js
+++ src/utils/dataParser.js
@@ -1,4 +1,6 @@
 import { exitFetch } from "./fetcher";
+import { Year, Month, Day, Subject } from "../models";
+
 
 jsonData = exitFetch();
 
@@ -6,10 +8,21 @@ daysArray = jsonData.days;
 subjectsArray = jsonData.subjects;
 
 
-const parseDays = (daysArray) => {
+const parseDays = (subjectsArray) => {
     year = new Year(2);
 
     daysArray.forEach(day => {
-        if(day.week)
+        if(day.subject_year === 2){
+            if(!(day.month in year.months)){
+                new Month(day.month);
+                year.addMonth(day.month);
+            }
+            if(!(day.day in year.months[day.month].days)){
+                new Day(day.day, day.month);
+                year.months[day.month].addDay(day.day);
+            }
+            newsubject = new Subject();
+            year.months[day.month].days[day.day].addSubject(day.subject);
+        }
     });
 }
